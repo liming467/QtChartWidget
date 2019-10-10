cb137cee1b4371c18bfab5e0cd01a38b6cfd38cc
Ming Li <53861552+liming467@users.noreply.github.com>
2019-10-10 20:41:48 +0800

    An interface for showing processed data and curve
    An interface for showing result data curve and corresponding two tables containing source/processed data .

---
+++ Splitter/mainwindow.h
@@ -0,0 +1,60 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <tableview.h>
+#include <chartview.h>
+#include <QTabWidget>
+#include <QSplitter>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+QT_CHARTS_USE_NAMESPACE
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+
+private:
+    Ui::MainWindow *ui;
+
+
+    QTabWidget *tabWidget;
+    TableView* tableView1;
+    QSplitter * splitter;
+    TableView* tableView2;
+    ChartView *m_chartview;    
+    QVector<QPointF> source_pointVector;
+    QVector<QPointF> post_pointVector;
+
+    void _Read_data();
+    void initTable();
+    void initChart();
+    void initSplitter();
+    void initTabWidget();
+
+
+    //Filter function plugin example
+    void Filter(QVector<QPointF> &src_pointVector,QVector<QPointF> &post_pointVector);
+
+
+    void medianFilter(QVector<QPointF> &src_pointVector,QVector<QPointF> &post_pointVector);
+    void averageFilter(QVector<QPointF> &src_pointVector,QVector<QPointF> &post_pointVector);
+
+
+
+
+
+};
+
+#endif // MAINWINDOW_H
